349d7ffc36e6a112f6968ee72f7e28e1fe8d9562
Guillaume Hivert <hivert.is.coming@gmail.com>
2017-07-09 20:31:11 +0200

    Correct application to follow new Proton

--- camera/main.rb
+++ camera/main.rb
@@ -3,14 +3,14 @@ require 'proton/browser_window'
 
 @app = Proton.app
 
-@app.on 'window-all-closed' do
+@app.on_window_all_closed do
   @app.quit if Process.platform != 'darwin'
 end
 
-@app.on 'ready' do
+@app.on_ready do
   Global.main_window = (Proton::BrowserWindow.new width: 800, height: 600).tap do |window|
     window.load_url "file://#{`__dirname`}/index.html"
-    window.on 'closed' do
+    window.on_closed do
       Global.main_window = nil
     end
   end
